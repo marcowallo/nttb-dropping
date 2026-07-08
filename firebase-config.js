8c421602a28065f8e3bddeb022f1255bf2bc24df
marcowallo <mavdwa@gmail.com>
2026-07-08 18:09:31 +0200

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,15 @@
+// Dropping v8 Firebase config.
+// Plak hier jouw Firebase web app config.
+// LET OP: gebruik deze compat-vorm, dus GEEN import-regels.
+
+window.DROPPING_FIREBASE_CONFIG = {
+  apiKey: "VUL_HIER_JE_API_KEY_IN",
+  authDomain: "VUL_HIER_JE_PROJECT.firebaseapp.com",
+  databaseURL: "https://VUL_HIER_JE_PROJECT-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "VUL_HIER_JE_PROJECT_ID_IN",
+  storageBucket: "VUL_HIER_JE_PROJECT.appspot.com",
+  messagingSenderId: "VUL_HIER_JE_SENDER_ID_IN",
+  appId: "VUL_HIER_JE_APP_ID_IN"
+};
+
+window.DROPPING_FIREBASE_PATH = "kamp2026";
