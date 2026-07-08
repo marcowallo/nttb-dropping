ed0a678e925eae220e663e979ce7b5a7c0d399a1
marcowallo <mavdwa@gmail.com>
2026-07-08 17:50:25 +0200

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,21 +1,11 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
+window.DROPPING_FIREBASE_CONFIG = {
   apiKey: "AIzaSyCwrQOYOY6cnTRCtblbh6GJcySzzHEw_vo",
   authDomain: "nttb-kamp-dropping.firebaseapp.com",
+  databaseURL: "HIER_KOMT_JE_DATABASE_URL",
   projectId: "nttb-kamp-dropping",
   storageBucket: "nttb-kamp-dropping.firebasestorage.app",
   messagingSenderId: "878956598499",
-  appId: "1:878956598499:web:afccee79533be49a4761d6",
-  measurementId: "G-7L2DBLEBNF"
+  appId: "1:878956598499:web:afccee79533be49a4761d6"
 };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+window.DROPPING_FIREBASE_PATH = "kamp2026";
