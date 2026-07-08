6ab69f239a12f7ada295bf8a770bde0bb86e3525
marcowallo <mavdwa@gmail.com>
2026-07-08 18:07:30 +0200

    Delete sw.js

--- sw.js
+++
@@ -1,9 +0,0 @@
-// Offline cache uitgeschakeld in v4.4.
-self.addEventListener("install", event => self.skipWaiting());
-self.addEventListener("activate", event => {
-  event.waitUntil(
-    caches.keys()
-      .then(keys => Promise.all(keys.map(key => caches.delete(key))))
-      .then(() => self.registration.unregister())
-  );
-});
