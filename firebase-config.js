86f1d06c029e6799a9a5e3b12e71fb4caacc4187
marcowallo <mavdwa@gmail.com>
2026-07-15 20:52:02 +0200

    Add files via upload

--- firebase-config.js
+++ firebase-config.js
@@ -2,13 +2,16 @@
 // Plak hier jouw Firebase web app config. Gebruik GEEN import-regels.
 
 window.DROPPING_FIREBASE_CONFIG = {
-  apiKey: "AIzaSyCwrQOYOY6cnTRCtblbh6GJcySzzHEw_vo",
-  authDomain: "nttb-kamp-dropping.firebaseapp.com",
-  databaseURL: "https://nttb-kamp-dropping-default-rtdb.europe-west1.firebasedatabase.app",
-  projectId: "nttb-kamp-dropping",
-  storageBucket: "nttb-kamp-dropping.firebasestorage.app",
-  messagingSenderId: "878956598499",
-  appId: "1:878956598499:web:afccee79533be49a4761d6"
+  apiKey: "VUL_HIER_JE_API_KEY_IN",
+  authDomain: "VUL_HIER_JE_PROJECT.firebaseapp.com",
+  databaseURL: "https://VUL_HIER_JE_PROJECT-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "VUL_HIER_JE_PROJECT_ID_IN",
+  storageBucket: "VUL_HIER_JE_PROJECT.appspot.com",
+  messagingSenderId: "VUL_HIER_JE_SENDER_ID_IN",
+  appId: "VUL_HIER_JE_APP_ID_IN"
 };
 
 window.DROPPING_EVENT_ID = "kamp2026";
+
+
+window.DROPPING_ADMIN_PASSWORD = "verander-mij";
